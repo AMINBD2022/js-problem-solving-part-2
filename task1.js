5f0f91c5acbf50691c7dd959354a8dba6a164f93
aminbd2022 <aminulxrpd@gmail.com>
2025-08-13 10:42:14 +0600

    added task 2

--- task1.js
+++ task1.js
@@ -3,3 +3,13 @@
 // Find the lowest number in the array below.
 // const heights2 = [167, 190, 120, 165, 137];
 
+
+
+const heights2 = [167, 190, 120, 165, 137];
+let result = 0;
+for (num of heights2){
+    if (result < num){
+        result = num;
+    }
+}
+console.log(result);
--- task2.js
+++ task2.js
@@ -1,3 +1,14 @@
 // Task -2:
 // Find the friend with the smallest name.
 // const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+
+
+
+const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+let singFriend = heights2[0];
+
+for (friend of heights2){
+    if (friend.length < singFriend.length){
+        singFriend = friend;
+    }
+}console.log(singFriend);
